3bcad4adcc3fc913ceba8181e85bf97f5fdcf091
ANIKET NEGI <aniketnegi.negi1@gmail.com>
2023-12-21 22:31:43 +0530

    Update Greeter.java
    Code check-in via git desktop app

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public static void main(String args[]) {
-        System.out.println("Hello everyone!!");
+        System.out.println("Hello everybody!!");
     }
 }
